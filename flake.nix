91b12b2c2667d7b4be4a8b2e16b31957e1148661
molarmanful <ben@benpa.ng>
2025-02-15 18:43:55 -0500

    simplify

--- flake.nix
+++ flake.nix
@@ -1,14 +1,8 @@
 {
-  description = "A fluorescent terminal colorscheme salvaged from the briny depths of the ocean.";
-
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  description = "A fluorescent terminal colorscheme salvaged from the briny depths of the ocean";
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-
+    { ... }:
     {
       lib = {
         wezterm = (builtins.fromTOML (builtins.readFile ./wezterm/abyssal.toml)).colors;
@@ -31,24 +25,5 @@
           base0F = "#c19ffc";
         };
       };
-    }
-
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nil
-            nixd
-            nixfmt-rfc-style
-            statix
-            deadnix
-            taplo
-          ];
-        };
-      }
-    );
+    };
 }
